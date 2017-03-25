ba29a862db79dcd31178f824c3fb3c065c469340
Christian Munte <cmunte132@gmail.com>
2017-03-25 11:49:38 -0400

    Pre and Post complete

--- Prog 3/main.cpp
+++ Prog 3/main.cpp
@@ -57,55 +57,11 @@ class set {
   private:
     vector<edge> connections;
   public:
-    set(int junctions){
-        for(int pos = 0; pos < junctions; pos++) {
-            connections.push_back(edge());
-        }
-    }
-    
-    int doFind(const vector<edge> input, int find) {
-        //preforms set find
-        //cout << "looking for " << find << endl;
-        if(input[find].parent >= 0) {
-            return doFind(input, input[find].parent);
-        }
-        return find;
-    }
-    
-    bool doUnion(int nodex, int nodey) {
-        //preforms set union true if cycle is made
-        int a = doFind(connections, nodex);
-        int b = doFind(connections, nodey);
-        if(a != b){
-            if(a < b) {
-                //cout << a << " is now " << b << "'s parent\n";
-                connections[b].parent = a;
-                connections[a].count += connections[b].count;
-            } else {
-                //cout << b << " is now " << a << "'s parent\n";
-                connections[a].parent = b;
-                connections[b].count += connections[a].count;
-            }
-        } else {
-            return true;
-        }
-        return false;
-    }
-    
-    bool isTree() {
-        int count = 0;
-        for(int index = 0; index < connections.size(); index ++) {
-            if(connections[index].parent != -1) {
-                count++;
-            }
-        }
-        if(count == connections.size() - 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
+    set(int);
+    int doFind(const vector<edge>, int);
+    bool doUnion(int, int);
+    bool isTree();
+    //PRE/POST NOT NEEDED
     void doPrint() {
         for (int pos = 0; pos < connections.size(); pos++) {
             cout << pos+1 << "   ";
@@ -128,65 +84,10 @@ class tree {
     vector<input> span;
     int junctions;
   public:
-    tree(int edges, int junct) {
-        int x, y, weight;
-        input temp;
-        
-        junctions = junct;
-        for (int num = 0; num < edges; num++) {
-            cin >> x >> y >> weight;
-            temp.node1 = x;
-            temp.node2 = y;
-            temp.weight = weight;
-            inputs.push_back(temp);
-        }
-    }
-    
-    int diff() {
-        sort(span.begin(), span.end());
-        return span.back().weight - span.front().weight;
-    }
-    
-    int doKruskal(vector<input> tree) {
-        set mst(junctions);
-        span.erase(span.begin(), span.end());
-        
-        while(tree.size() > 0 && span.size() < junctions - 1) {
-            //uses kruskal Algo
-            if(!mst.doUnion(tree.front().node1 - 1, tree.front().node2 - 1)) {
-                //if it doesn't make a cycle
-                span.push_back(tree.front());
-                tree.erase(tree.begin());
-            } else {
-                tree.erase(tree.begin());
-            }
-        }
-        //mst.doPrint();
-        if(mst.isTree()) {
-            return diff();
-        } else {
-            return -1;
-        }
-    }
-
-    int findTree() {
-        int smallest = INT_MAX, result;
-        sort(inputs.begin(), inputs.end());
-        
-        while(inputs.size() >= junctions - 1) {
-            result = doKruskal(inputs);
-            //cout << result << " | " << smallest << endl;
-            if (result < smallest && result != -1) {
-                smallest = doKruskal(inputs);
-            }
-            inputs.erase(inputs.begin());
-        }
-        if (smallest == INT_MAX) {
-            return -1;
-        }
-        return smallest;
-    }
-
+    tree(int, int);
+    int doKruskal(vector<input>);
+    int findTree();
+    int diff();
 };
 
 
@@ -205,4 +106,136 @@ int main() { //4 5 1 2 3 1 3 5 1 4 6 2 4 6 3 4 7 0 0
         cout << inpt[index].findTree() << endl;
     }
     return 0;
+}
+
+//BEGIN SET CLASS
+set::set(int junctions) {
+    //PRE: Junctions >= 2
+    //POST: junctions connections are made for set
+    //constructor, clean
+    for(int pos = 0; pos < junctions; pos++) {
+        connections.push_back(edge());
+    }
+}
+
+int set::doFind(const vector<edge> input, int find) {
+    //PRE: Input is initialized, find is 0 index and >= 0
+    //POST: returns 0 index location of parent
+    //preforms set find
+    //cout << "looking for " << find << endl;
+    if(input[find].parent >= 0) {
+        return doFind(input, input[find].parent);
+    }
+    return find;
+}
+
+bool set::doUnion(int nodex, int nodey) {
+    //PRE: nodex >= 0, nodey >= 0 and both are 0 index and connections is initialized
+    //POST: true if cycle, false if not. set union is made to connections vector
+    //preforms set union true if cycle is made
+    int a = doFind(connections, nodex);
+    int b = doFind(connections, nodey);
+    if(a != b){
+        if(a < b) {
+            //cout << a << " is now " << b << "'s parent\n";
+            connections[b].parent = a;
+            connections[a].count += connections[b].count;
+        } else {
+            //cout << b << " is now " << a << "'s parent\n";
+            connections[a].parent = b;
+            connections[b].count += connections[a].count;
+        }
+    } else {
+        return true;
+    }
+    return false;
+}
+
+bool set::isTree() {
+    //PRE: connections is initialized
+    //POST: return true if valid and false if not valid
+    //check to see if set is a valid spanning tree
+    int count = 0;
+    for(int index = 0; index < connections.size(); index ++) {
+        if(connections[index].parent != -1) {
+            count++;
+        }
+    }
+    if(count == connections.size() - 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//BEGIN TREE CLASS
+tree::tree(int edges, int junct) {
+    //PRE: edges 0 <= junct(junct-1)/2 and junct >= 2
+    //POST: inputs is cully initialized with input
+    //constructor and gets input
+    int x, y, weight;
+    input temp;
+    
+    junctions = junct;
+    for (int num = 0; num < edges; num++) {
+        cin >> x >> y >> weight;
+        temp.node1 = x;
+        temp.node2 = y;
+        temp.weight = weight;
+        inputs.push_back(temp);
+    }
+}
+
+int tree::doKruskal(vector<input> tree) {
+    //PRE: tree contains >= junctions - 1 input elements
+    //POST: returns -1 if not valid tree or the diff if valid tree
+    //runs kruskal's algo
+    set mst(junctions);
+    span.erase(span.begin(), span.end());
+    
+    while(tree.size() > 0 && span.size() < junctions - 1) {
+        //uses kruskal Algo
+        if(!mst.doUnion(tree.front().node1 - 1, tree.front().node2 - 1)) {
+            //if it doesn't make a cycle
+            span.push_back(tree.front());
+            tree.erase(tree.begin());
+        } else {
+            tree.erase(tree.begin());
+        }
+    }
+    //mst.doPrint();
+    if(mst.isTree()) {
+        return diff();
+    } else {
+        return -1;
+    }
+}
+
+int tree::findTree() {
+    //PRE: inputs is initialized
+    //POST: returns the diff of the smellest tree
+    //find the smallest tree
+    int smallest = INT_MAX, result;
+    sort(inputs.begin(), inputs.end());
+    
+    while(inputs.size() >= junctions - 1) {
+        result = doKruskal(inputs);
+        //cout << result << " | " << smallest << endl;
+        if (result < smallest && result != -1) {
+            smallest = doKruskal(inputs);
+        }
+        inputs.erase(inputs.begin());
+    }
+    if (smallest == INT_MAX) {
+        return -1;
+    }
+    return smallest;
+}
+
+int tree::diff() {
+    //PRE: span is initialized
+    //POST: returns the max weight - min weight in span
+    //finds the diff of the span
+    sort(span.begin(), span.end());
+    return span.back().weight - span.front().weight;
 }
